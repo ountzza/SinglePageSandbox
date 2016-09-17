b9659b55360dbf49f950891690547fd73069a33c
Pondd <pond1982a@gmail.com>
2016-09-17 11:12:03 +0700

    more stuff

--- SinglePageSandbox/ViewController.swift
+++ SinglePageSandbox/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var spinButton: UIButton!
+    @IBOutlet weak var TitleLable: UILabel!
+    @IBOutlet weak var diceFace: UILabel!
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("this is loading")
+        textField.resignFirstResponder()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PressButton(_ sender: AnyObject) {
+        let diceRoll = Int(arc4random_uniform(6) + 1)
+        diceFace.text = "\(diceRoll)"
+      
+    }
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        sliderLabel.text = "\(sender.value)"
+    }
+   
+
+    @IBAction func buttonsPressed(_ sender: AnyObject) {
+        print(sender.tag)
+    }
 
 }
 
